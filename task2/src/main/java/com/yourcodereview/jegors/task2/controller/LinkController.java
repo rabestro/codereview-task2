ea37a0363f952f263ccebd7be99a8bbd5bae7bab
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-06-25 18:27:17 +0300

    Add LinkController

---
+++ task2/src/main/java/com/yourcodereview/jegors/task2/controller/LinkController.java
@@ -0,0 +1,21 @@
+package com.yourcodereview.jegors.task2.controller;
+
+import com.yourcodereview.jegors.task2.dto.GenerateRequest;
+import com.yourcodereview.jegors.task2.dto.GenerateResponse;
+import com.yourcodereview.jegors.task2.service.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class LinkController {
+
+    @Autowired
+    LinkService service;
+
+    public GenerateResponse generate(@RequestBody @Valid GenerateRequest request) {
+        return new GenerateResponse(service.generate(request.original()));
+    }
+}
