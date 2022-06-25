4e57a8dea14829e28d6b84682289202c7b631169
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-06-25 18:27:38 +0300

    Add LinkServiceImpl

---
+++ task2/src/main/java/com/yourcodereview/jegors/task2/service/LinkServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yourcodereview.jegors.task2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LinkServiceImpl implements LinkService {
+
+    @Override
+    public String generate(String original) {
+        return null;
+    }
+}
